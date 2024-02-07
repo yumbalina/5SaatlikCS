1c816305f92f453770d11b2c14af9a4f15f3061e
unknown <gorener.cem@gmail.com>
2024-02-07 03:12:03 +0300

    Fonksiyonlara Kadar Olan Kisim

---
+++ MaskeTakip/Business/Concrete/PersonManager.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Concrete Klasoru soyut nesneleri ve ona ait olacak islemleri tutar
+namespace Business.Concrete
+{
+    public class PersonManager
+    {
+        public void ApplyForMask(Person person) { }
+    }
+}
+    
---
+++ MaskeTakip/WorkAround/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+//static void degiskenler()
+//{
+//    string mesaj = "Merhaba!";
+//    double tutar = 100000; //dbden gelir
+//    int sayi = 100;
+//    bool girisYapmisMi = false;
+
+//    string ad = "Engin";
+//    string soyad = "Demiroğ";
+//    int dogumYili = 1985;
+//    long tcNo = 11111111111;
+
+//    //Projeye Bagli
+//    Console.WriteLine(tutar * 1.18);
+//}
+//public class Vatandas
+//{
+//    public string Ad { get; set; }
+//    public string Soyad { get; set; }
+//    public int DogumYili { get; set; }
+//    public long TcNo { get; set; }
+//}
+
+//degiskenler();
+//Vatandas vatandas1 = new Vatandas();
+
+static void SelamVer() 
+{
+    Console.WriteLine("Merhaba!");
+}
+SelamVer();
+static int Topla()
+{
+    return 5;
+}
+int sonuc = Topla();
+
