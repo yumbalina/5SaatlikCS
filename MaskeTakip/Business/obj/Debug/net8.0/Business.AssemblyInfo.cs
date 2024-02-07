032692030323153c9d8adb9b1f578b5d5d9b6bf6
yumbalina <gorener.cem@gmail.com>
2024-02-07 06:57:02 +0300

    Program uzerinde tc sorgusu

--- MaskeTakip/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ MaskeTakip/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7af9c9238884148086e1bfac655459f2731f2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee154fbdf4cb14bcd683d3678cc869baf8587a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
